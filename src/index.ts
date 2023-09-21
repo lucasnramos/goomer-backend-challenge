abdcf85d5d40025f28944da9a08898dee92a6f15
Lucas Ramos <lucas.ramos@gft.com>
2023-09-21 09:47:00 -0300

    chore: setup dev env with nodemon, dotenv and ts-node

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("hello")
+import express, { Request, Response } from "express";
+import "dotenv/config";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (_: Request, res: Response) => {
+  res.status(200).send("Done!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
