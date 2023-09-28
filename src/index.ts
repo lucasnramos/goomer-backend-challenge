d7bae18e5876333dbeefeac7f121a5e4a4809de7
Lucas Ramos <lucas.ramos@gft.com>
2023-09-28 07:55:17 -0300

    feat: add MySQL driver and some initial queries

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,82 @@
 import express, { Request, Response } from "express";
 import "dotenv/config";
+import bodyParser from "body-parser";
+import { MysqlError, createConnection } from "mysql";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(bodyParser.json());
 
-app.get("/", (_: Request, res: Response) => {
-  res.status(200).send("Done!");
+const connection = createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+});
+connection.connect();
+
+app.get("/restaurant", (_: Request, res: Response) => {
+  connection.query(
+    "SELECT * FROM restaurant",
+    (err: MysqlError | null, results?: Restaurant[]) => {
+      if (err) throw err;
+      res.status(200).json(results);
+    }
+  );
+});
+
+app.post("/restaurant", (req: Request, res: Response) => {
+  const restaurant: Restaurant = req.body;
+
+  connection.query(
+    `INSERT INTO restaurant (name, address, business_hours, picture_url) VALUES ('${restaurant.name}', '${restaurant.address}', '${restaurant.businessHours}', '${restaurant.picture}')`,
+    (err, dbRes) => {
+      if (err) {
+        res.status(500);
+        throw err;
+      }
+      console.log("Added to database");
+      res.status(200).json(dbRes);
+    }
+  );
+});
+
+app.get("/product", (_: Request, res: Response) => {
+  const product: Product = {
+    id: 1,
+    picture: "", // probably a link to CDN file
+    name: "Batata frita",
+    price: 10,
+    category: "Acompanhamentos", // maybe ID to ref a category model / table
+    restaurantId: 1,
+  };
+  res.status(200).json(product);
 });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+type Restaurant = {
+  id?: number;
+  name: string;
+  address: string;
+  businessHours: string;
+  picture: string; // probably a link to CDN file
+};
+
+type Product = {
+  id?: number;
+  picture: string; // probably a link to CDN file
+  name: string;
+  price: number;
+  category: string; // maybe ID to ref a category model / table
+  restaurantId: number;
+  discount?: Discount;
+};
+
+type Discount = {
+  productId: number;
+  description: string;
+  discountedPrice: number;
+  period: string;
+};
