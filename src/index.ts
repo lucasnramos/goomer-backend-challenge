159218022124819364044231871240d49622281d
Lucas Ramos <lucas.ramos@gft.com>
2023-09-28 09:55:28 -0300

    feat: add inital put and delete for restaurant

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,10 @@ app.get("/restaurant", (_: Request, res: Response) => {
   connection.query(
     "SELECT * FROM restaurant",
     (err: MysqlError | null, results?: Restaurant[]) => {
-      if (err) throw err;
+      if (err) {
+        res.sendStatus(500);
+        throw err;
+      }
       res.status(200).json(results);
     }
   );
@@ -26,15 +29,44 @@ app.get("/restaurant", (_: Request, res: Response) => {
 
 app.post("/restaurant", (req: Request, res: Response) => {
   const restaurant: Restaurant = req.body;
-
   connection.query(
     `INSERT INTO restaurant (name, address, business_hours, picture_url) VALUES ('${restaurant.name}', '${restaurant.address}', '${restaurant.businessHours}', '${restaurant.picture}')`,
     (err, dbRes) => {
       if (err) {
-        res.status(500);
+        res.sendStatus(500);
         throw err;
       }
       console.log("Added to database");
+      res.status(200).json(dbRes);
+    }
+  );
+});
+
+app.put("/restaurant", (req: Request, res: Response) => {
+  const restaurant: Restaurant = req.body;
+  connection.query(
+    `UPDATE restaurant
+      SET name ='${restaurant.name}', address = '${restaurant.address}', business_hours = '${restaurant.businessHours}', picture_url='${restaurant.picture}'
+    WHERE id = ${restaurant.id}`,
+    (err, dbRes) => {
+      if (err) {
+        res.sendStatus(500);
+        throw err;
+      }
+      console.log("Updated record of id", restaurant.id);
+      res.status(200).json(dbRes);
+    }
+  );
+});
+
+app.delete("/restaurant", (req: Request, res: Response) => {
+  connection.query(
+    `DELETE FROM restaurant WHERE id=${req.query.id}`,
+    (err, dbRes) => {
+      if (err) {
+        res.sendStatus(500);
+        throw err;
+      }
       res.status(200).json(dbRes);
     }
   );
