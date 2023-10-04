4e4a1bea9cfda1c47a83db96abf67be0bf1637ce
Lucas Ramos <lucas.ramos@gft.com>
2023-10-04 16:10:42 -0300

    feat: add GET for restaurant

--- src/repositories/restaurant-repository.ts
+++ src/repositories/restaurant-repository.ts
@@ -1,8 +1,30 @@
+import { Pool, PoolConnection } from "mysql";
+import { promisify } from "util";
 import Restaurant from "../entities/restaurant.js";
 
 export interface IRestaurantRespository {
   findAll(): Promise<Restaurant[]>;
   create(props: Restaurant): Promise<void>;
-  update(props: Restaurant): Promise<void>;
-  delete(id: number): Promise<void>;
+}
+
+export class MySQLRestaurantRepository implements IRestaurantRespository {
+  constructor(private dataSource: Pool) {}
+
+  async findAll(): Promise<Restaurant[]> {
+    const getConnection = promisify(this.dataSource.getConnection).bind(
+      this.dataSource
+    );
+    const query = promisify(this.dataSource.query).bind(this.dataSource);
+
+    try {
+      const connection: PoolConnection = await getConnection();
+      const results = (await query("SELECT * FROM restaurant")) as Restaurant[];
+      connection.release();
+      return results;
+    } catch (err) {
+      console.error("Error in findAll: ", err);
+      throw err;
+    }
+  }
+
 }
