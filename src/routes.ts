7c10e0e613a73a1b4c194223d6aba5e514011871
Lucas Ramos <lucas.ramos@gft.com>
2023-10-04 16:10:13 -0300

    feat: initial express routing with mysql adapter

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,29 @@
 import { Router } from "express";
+import pool from "./adapters/mysql-adapter.js";
+import {
+  IRestaurantRespository,
+  MySQLRestaurantRepository,
+} from "./repositories/restaurant-repository.js";
+import GetAllRestaurantsUseCase from "./use-cases/restaurant-use-cases.js";
 
 const router = Router();
 
+const dataSource = pool;
+let repository: IRestaurantRespository = new MySQLRestaurantRepository(
+  dataSource
+);
+let getAllRestaurants: GetAllRestaurantsUseCase = new GetAllRestaurantsUseCase(
+  repository
+);
+
 router.get("/restaurants", (_, res) => {
-  console.log("hello from api/v1");
-  res.sendStatus(200);
+  try {
+    const restaurants = getAllRestaurants.execute();
+    res.send(restaurants);
+  } catch (error) {
+    console.error("Failed to retrieve data", error);
+    res.status(500).send("Failed to retrieve data");
+  }
 });
 router.get("/restaurants/:id", (_, res) => {
   console.log("hello from api/v1");
