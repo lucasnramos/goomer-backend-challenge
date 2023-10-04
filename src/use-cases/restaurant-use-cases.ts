5b015904ac5d86e9cd1664133036c319b4088364
Lucas Ramos <lucas.ramos@gft.com>
2023-10-04 17:34:28 -0300

    feat: first validation on create restaurant

--- src/use-cases/restaurant-use-cases.ts
+++ src/use-cases/restaurant-use-cases.ts
@@ -13,6 +13,16 @@ export class CreateRestaurantsUseCase {
   constructor(private restaurantRepository: IRestaurantRespository) {}
 
   async execute(restaurant: Restaurant): Promise<void> {
+    const restaurants = await new GetAllRestaurantsUseCase(
+      this.restaurantRepository
+    ).execute();
+    const hasRestaurant =
+      restaurants.findIndex((item) => item.name === restaurant.name) > -1;
+
+    if (hasRestaurant) {
+      throw new Error(`Restaurant ${restaurant.name} already exists`);
+    }
+
     return await this.restaurantRepository.create(restaurant);
   }
 }
